429c1eaa038d854eca980dbdb4878f064646a9f8
charleshenricks <charleshenricks@gmail.com>
2023-02-25 03:59:21 +0800

    Charles commit

--- lib/verified/verified.dart
+++ lib/verified/verified.dart
@@ -3,78 +3,62 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:proj3/homepage/homepage.dart';
 
-
-
-class MyVerified extends StatelessWidget{
+class MyVerified extends StatelessWidget {
   const MyVerified({super.key});
 
-  @override 
-  Widget build(BuildContext context){
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body : SafeArea(
+      body: SafeArea(
         child: SingleChildScrollView(
           child: Center(
-            child : Column(
-              children:  [
-                const SizedBox(height : 34),
-                
+            child: Column(
+              children: [
+                const SizedBox(height: 34),
+
                 //Logo check
                 SvgPicture.asset('lib/svg/veriflogo.svg'),
-                
 
                 //Email Text
-                const SizedBox(height  : 109.43 ),
-                Text(
-                  'Email Address Verfied',
-                  style : GoogleFonts.poppins(
-                    fontSize : 24, 
-                    fontWeight: FontWeight.bold
-                  )
-                  
-
-                ),
-                const SizedBox(height : 16),
-                
-                Container(
-                margin : const EdgeInsets.fromLTRB(47, 0 , 62, 0  ),
-                  child: Text(
-                    'Congratulations, your account has been verified. You can start using the app',
-                    textAlign: TextAlign.center,
+                const SizedBox(height: 109.43),
+                Text('Email Address Verfied',
                     style: GoogleFonts.poppins(
-                      fontSize: 16,
-                      height: 1.5,    
-                      color: Colors.grey[600],
-                    )
-                  ),
+                        fontSize: 24, fontWeight: FontWeight.bold)),
+                const SizedBox(height: 16),
+
+                Container(
+                  margin: const EdgeInsets.fromLTRB(47, 0, 62, 0),
+                  child: Text(
+                      'Congratulations, your account has been verified. You can start using the app',
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.poppins(
+                        fontSize: 16,
+                        height: 1.5,
+                        color: Colors.grey[600],
+                      )),
                 ),
 
-                  //Continue Button
-                  const SizedBox(height : 109),
+                //Continue Button
+                const SizedBox(height: 109),
 
-                  GestureDetector(
-                    onTap: (){
-                      Navigator.of(context).push(MaterialPageRoute(builder: (context) => const Myhomepage()));
-                    },
-                    child : Center(
-                      child: Container(
-                        padding: const EdgeInsets.fromLTRB(108, 13, 108, 13),
-                        
-                        decoration: const  BoxDecoration(
-                          color: Color(0xff6C63FF),
-                          borderRadius: BorderRadius.all(Radius.circular(10)),
-                        ),
-                        child:  Text(
-                          'Continue',
-                          style: GoogleFonts.poppins(
-                            fontSize: 20,
-                            color: Colors.white
-                          
-                          )
-                      
-                        ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => const Myhomepage()));
+                  },
+                  child: Center(
+                    child: Container(
+                      padding: const EdgeInsets.fromLTRB(108, 13, 108, 13),
+                      decoration: const BoxDecoration(
+                        color: Color(0xff6C63FF),
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
                       ),
+                      child: Text('Continue',
+                          style: GoogleFonts.poppins(
+                              fontSize: 20, color: Colors.white)),
                     ),
-                  )
+                  ),
+                )
               ],
             ),
           ),
@@ -83,3 +67,5 @@ class MyVerified extends StatelessWidget{
     );
   }
 }
+
+//Hi this is a comment  that I want to see the progress in github
