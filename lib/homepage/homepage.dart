1ab32b0f3b6da709ae0cff1dd52c21dfac731750
David <dpa@crafttec.ai>
2023-03-04 09:37:47 +0800

    Fixed -- sticking app bar --

--- lib/homepage/homepage.dart
+++ lib/homepage/homepage.dart
@@ -35,183 +35,202 @@ class _MyhomepageState extends State<Myhomepage> {
       extendBody: true,
       resizeToAvoidBottomInset: false,
 
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: const Color(0xff6C63FF),
-      ),
-
       drawer: Drawer(
-          child: SingleChildScrollView(
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-            buildHeader(context),
-            buildMenuItems(context),
-          ]))),
-
-      body: SafeArea(
         child: SingleChildScrollView(
           child: Column(
-            children: [
-              Stack(
-                children: <Widget>[
-                  Container(
-                    height: 179,
-                    decoration: const BoxDecoration(
-                        color: Color(0xff6C63FF),
-                        borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(48),
-                          bottomRight: Radius.circular(48),
-                        )),
-                  ),
-
-                  //header
-                  Center(
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(0, 52, 0, 46),
-                      child: Column(
-                        children: [
-                          Text(
-                            "Lend, borrow, repeat",
-                            style: GoogleFonts.poppins(
-                              fontSize: 20,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text('with ease!',
-                              style: GoogleFonts.poppins(
-                                fontSize: 20,
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold,
-                              )),
-                        ],
-                      ),
-                    ),
-                  ),
-
-                  Center(
-                    child: Padding(
-                        padding: const EdgeInsets.fromLTRB(45, 148, 45, 0),
-                        child: GestureDetector(
-                          onTap: () {
-                            Navigator.of(context).push(MaterialPageRoute(
-                                builder: (context) => MySearchPage()));
-                          },
-                          child: MysearchBar(
-                              controller: searchController,
-                              hinttext: "Search for Everything",
-                              icon: const Icon(Icons.search,
-                                  color: Color(0xffA5A3A3))),
-                        )),
-                  ),
-                ],
-              ),
-
-              //categories
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 26, top: 28),
-                    child: Text(
-                      "Categories",
-                      style: GoogleFonts.poppins(fontSize: 15),
-                    ),
-                  ),
-                ],
-              ),
-
-              //buttons under categories
-              Padding(
-                padding: const EdgeInsets.fromLTRB(28, 11, 28, 0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    MyCategoryBtn(
-                      btnName: "Textbooks",
-                      svgName: 'assets/svg/book-alert-outline.svg',
-                      btnClicked: btnClicked,
-                    ),
-                    MyCategoryBtn(
-                      btnName: "Electronics",
-                      svgName: 'assets/svg/electronics-logo.svg',
-                      btnClicked: btnClicked,
-                    ),
-                    MyCategoryBtn(
-                      btnName: "Sports",
-                      svgName: 'assets/svg/sports-logo.svg',
-                      btnClicked: btnClicked,
-                    ),
-                    MyCategoryBtn(
-                      btnName: "Music",
-                      svgName: 'assets/svg/music-logo.svg',
-                      btnClicked: btnClicked,
-                    ),
-                  ],
-                ),
-              ),
-
-              //Lend Listings and Borrow Listings
-              const SizedBox(height: 20),
-              const Divider(
-                thickness: 1,
-                color: Colors.black,
-              ),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        borrowisClicked =
-                            false; //remove the color of borrow req
-                        lendisClicked =
-                            !lendisClicked; //highlight the Lend list
-                        textName = 'listings';
-                      });
-                      print("Test for Lend");
-                    },
-                    child: Mylendborrowoptn(
-                      lBName: "Lend Listings",
-                      lendBorrowisClicked: lendisClicked,
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        lendisClicked = false; //remove color Lend List
-                        borrowisClicked =
-                            !borrowisClicked; //highlight the lend list
-                        textName = 'requests';
-                      });
-                      print("Test for Borrow");
-                    },
-                    child: Mylendborrowoptn(
-                      lBName: "Borrow Request",
-                      lendBorrowisClicked: borrowisClicked,
-                    ),
-                  ),
-                ],
-              ),
-
-              const Divider(
-                thickness: 1,
-                color: Colors.black,
-              ),
-
-              const SizedBox(height: 38),
-              SvgPicture.asset('assets/svg/no_items-logo.svg'),
-              Text(
-                "No $textName yet.",
-                style: GoogleFonts.poppins(
-                  fontSize: 15,
-                ),
-              ),
-              const SizedBox(height: 500),
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildHeader(context),
+              buildMenuItems(context),
             ],
           ),
         ),
+      ),
+
+      body: Builder(
+        builder: (context) {
+          return SafeArea(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Stack(
+                    children: <Widget>[
+                      Container(
+                        height: 239,
+                        decoration: const BoxDecoration(
+                          color: Color(0xff6C63FF),
+                          borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(48),
+                            bottomRight: Radius.circular(48),
+                          ),
+                        ),
+                      ),
+
+                      //header
+                      Center(
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 112, 0, 46),
+                          child: Column(
+                            children: [
+                              Text(
+                                "Lend, borrow, repeat",
+                                style: GoogleFonts.poppins(
+                                  fontSize: 20,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text('with ease!',
+                                  style: GoogleFonts.poppins(
+                                    fontSize: 20,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                  )),
+                            ],
+                          ),
+                        ),
+                      ),
+
+                      Center(
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(45, 208, 45, 0),
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) => const MySearchPage(),
+                                ),
+                              );
+                            },
+                            child: MysearchBar(
+                              controller: searchController,
+                              hinttext: "Search for Everything",
+                              icon: const Icon(
+                                Icons.search,
+                                color: Color(0xffA5A3A3),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        top: 10,
+                        left: 5,
+                        child: IconButton(
+                          onPressed: () => Scaffold.of(context).openDrawer(),
+                          icon: const Icon(Icons.menu),
+                          color: Colors.white,
+                          iconSize: 30,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  //categories
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 26, top: 28),
+                        child: Text(
+                          "Categories",
+                          style: GoogleFonts.poppins(fontSize: 15),
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  //buttons under categories
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(28, 11, 28, 0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        MyCategoryBtn(
+                          btnName: "Textbooks",
+                          svgName: 'assets/svg/book-alert-outline.svg',
+                          btnClicked: btnClicked,
+                        ),
+                        MyCategoryBtn(
+                          btnName: "Electronics",
+                          svgName: 'assets/svg/electronics-logo.svg',
+                          btnClicked: btnClicked,
+                        ),
+                        MyCategoryBtn(
+                          btnName: "Sports",
+                          svgName: 'assets/svg/sports-logo.svg',
+                          btnClicked: btnClicked,
+                        ),
+                        MyCategoryBtn(
+                          btnName: "Music",
+                          svgName: 'assets/svg/music-logo.svg',
+                          btnClicked: btnClicked,
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  //Lend Listings and Borrow Listings
+                  const SizedBox(height: 20),
+                  const Divider(
+                    thickness: 1,
+                    color: Colors.black,
+                  ),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            borrowisClicked =
+                                false; //remove the color of borrow req
+                            lendisClicked =
+                                !lendisClicked; //highlight the Lend list
+                            textName = 'listings';
+                          });
+                          print("Test for Lend");
+                        },
+                        child: Mylendborrowoptn(
+                          lBName: "Lend Listings",
+                          lendBorrowisClicked: lendisClicked,
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            lendisClicked = false; //remove color Lend List
+                            borrowisClicked =
+                                !borrowisClicked; //highlight the lend list
+                            textName = 'requests';
+                          });
+                          print("Test for Borrow");
+                        },
+                        child: Mylendborrowoptn(
+                          lBName: "Borrow Request",
+                          lendBorrowisClicked: borrowisClicked,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  const Divider(
+                    thickness: 1,
+                    color: Colors.black,
+                  ),
+
+                  const SizedBox(height: 38),
+                  SvgPicture.asset('assets/svg/no_items-logo.svg'),
+                  Text(
+                    "No $textName yet.",
+                    style: GoogleFonts.poppins(
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
       ),
 
       // for bottom Navigation
